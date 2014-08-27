7a35176b13c59d323857688682dd3b1485477b37
Anand Kuchibotla <akuchibotla@gmail.com>
2014-08-26 22:43:04 -0700

    Added a bust of myself to the 'About Me' page

--- aboutme.js
+++ aboutme.js
@@ -1,9 +1,21 @@
 $("h1").css({"opacity":"1"}).shuffleLetters();
 $(document).ready(function() {
+	console.log("READY!");
 	var img_array = [];
 
-	for (var i = 0; i < 46; i++){
-		img_array.push("cookie_frames/" + i + ".gif");
+	for (var i = 0; i < 450; i+=9){
+		if (i < 10) {
+			console.log("HERE 1");
+			img_array.push("me/me_00" + i + ".png");
+		}
+		else if (i < 100) {
+			console.log("HERE 2");
+			img_array.push("me/me_0" + i + ".png");
+		}
+		else {
+			console.log("HERE 3");
+			img_array.push("me/me_" + i + ".png");
+		}
 	}
 	$("#mousemove").css({"visibility":"visible", "opacity":"0"}).animate({
 		opacity: "1"
