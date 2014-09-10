3bff869de0411add43bc3ce06e69378269d4e493
Anand Kuchibotla <akuchibotla@gmail.com>
2014-09-09 17:05:05 -0700

    Fixed rotating head

--- aboutme.js
+++ aboutme.js
@@ -4,34 +4,9 @@ $(document).ready(function() {
 	var img_array = [];
 
 	var prefix = 0
-	for (var i = 0; i < 450; i+=9){
-		if (i < 10) {
-			if (prefix < 10) {
-				img_array.push("me_edited/_000" + prefix + "_me_00" + i + ".png");
-				console.log(img_array.push("me_edited/_000" + prefix + "_me_00" + i + ".png"));
-			}
-			else {
-				img_array.push("me_edited/_00" + prefix + "_me_00" + i + ".png");
-				//console.log(img_array.push("me_edited/_00" + prefix + "_me_00" + i + ".png"));
-			}
-		}
-		else if (i < 100) {
-			if (prefix < 10) {
-				img_array.push("me_edited/_000" + prefix + "_me_0" + i + ".png");
-				//console.log(img_array.push("me_edited/_000" + prefix + "_me_0" + i + ".png"));
-			}
-			else {
-				img_array.push("me_edited/_00" + prefix + "_me_0" + i + ".png");
-				//console.log(img_array.push("me_edited/_00" + prefix + "_me_0" + i + ".png"));
-			}
-		}
-		else {
-			img_array.push("me_edited/_00" + prefix + "_me_" + i + ".png");
-			console.log(img_array.push("me_edited/_00" + prefix + "_me_" + i + ".png"));
-		}
-
-		prefix ++;
-	}
+	for (var i = 0; i < 52; i++){
+		img_array.push("me_contoured/" + i + ".png");
+ 	}
 	$("#mousemove").css({"visibility":"visible", "opacity":"0"}).animate({
 		opacity: "1"
 	}, 500);
