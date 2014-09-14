3a4520b925919bd5b51cb81f17a7137e81c6dbd4
Anand Kuchibotla <akuchibotla@gmail.com>
2014-09-13 19:40:20 -0700

    Added more to Things I'm Proud Of

--- scripts/thingsimproudof.js
+++ scripts/thingsimproudof.js
@@ -1,7 +1,15 @@
 $(function(){
-	space = "        ";
+	$("#skip").animate({opacity: 1}, 2000).click(appear);
+	$("body").toggleClass("bgblack");
+	space = "    ";
+	string_possibilities = [
+		["WAH", "WHAT AM I PROUD OF?" + space, "ABSOLUTELY NOTHING" + space, "JUST KIDDING" + space, "CHECK OUT MY WORK" + space],
+		["WHAT AM I PROUD OF?" + space, "ASOL", "ABSOLUTELY NOTHING" + space, "JUST KIDDING" + space, "CHECK OUT MY WORK" + space],
+		["WHAT AM I PROUD OF?" + space, "ABSOLUTELY NOTHING" + space, "JOST", "JUST KIDDING" + space, "CHECK OUT MY WORK" + space],
+		["WHAT AM I PROUD OF?" + space, "ABSOLUTELY NOTHING" + space, "JUST KIDDING" + space, "CEHC", "CHECK OUT MY WORK" + space]
+	];
   	$(".element").typed({
-		strings: ["ABSOLUTELY NOTHING" + space, "JOST", "JUST KIDDING" + space, "CHECK OUT MY WORK"],
+		strings: string_possibilities[Math.floor(Math.random() * string_possibilities.length)],
 		typeSpeed: 20, // typing speed
 		backSpeed: 0, // backspacing speed
 		startDelay: 10, // time before typing starts
@@ -16,7 +24,8 @@ $(function(){
 
 appear = function() {
 	$(".element").fadeOut(1000);
-	$("body").delay(1000).toggleClass("bodychanged");
-	header1 = "<center> <h1> THINGS I'M PROUD OF </h1> <center>"
+	$("#skip").fadeOut(1000);
+	$("body").toggleClass("bgwhite");
+	header1 = "<center> <h1> THINGS I'M PROUD OF </h1> <center>";
 	$(header1).delay(1000).hide().appendTo("body").fadeIn(1000);
 }
